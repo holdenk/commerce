401a8546aa547919d392ac9b82a4f1bb0b3dbaea
Andrew Premdas <apremdas@yahoo.co.uk>
2011-06-27 15:36:33 +0100

    ran 'rails new . --skip-bundle -d mysql -T'
    installing rails without testing (will use rspec), using mysql
    did small update to Gemfile before running bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commerce::Application.config.secret_token = '415559404cb23a99c946c9a1c87a169eb9b80befafae6be101f60de2c4879d5f511676525a39986d5e525c870bf1e5181cd510522c2e2ed7c45c11de4797113e'
